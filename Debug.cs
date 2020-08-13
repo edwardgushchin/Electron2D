c0c2a84d350694e2d32cba0d5d9ee0b3084167f0
eduardgushchin <eduardgushchin@yandex.ru>
2020-08-13 13:47:00 +1100

    Добавил функционал для вывода сообщений от Physics и фукнционал отрисовки примитивов

--- Debug.cs
+++ Debug.cs
@@ -19,6 +19,7 @@ namespace Electron2D
 		public enum Sender {
 			Main,
 			Render,
+			Physics,
 			Event,
 			SceneManager,
 			Scene,
@@ -88,6 +89,73 @@ namespace Electron2D
 			}
 			Console.WriteLine(Message);
 			Console.ResetColor();
+		}
+
+		public static void DrawText()
+		{
+
+		}
+
+
+		public static void DrawPoint()
+		{
+
+		}
+
+		public static void DrawPoints()
+		{
+
+		}
+
+		public static void DrawLine()
+		{
+
+		}
+
+		public static void DrawLines()
+		{
+
+		}
+
+		public static void DrawRectangle()
+		{
+
+		}
+
+		public static void DrawRectangles()
+		{
+
+		}
+
+		public static void DrawGrid()
+		{
+			var color = SceneManager.GetCurrentScene.ClearColor;
+			var camera = SceneManager.GetCurrentScene.Camera;
+			var size = new Graphics.Rect(100, 100);
+
+			Binding.SDL.SDL.SDL_SetRenderDrawColor(Game.RenderContext, 100, 100, 100, 0);
+
+			for (int i = -100; i <= 100; i++)
+			{
+				var p1 = camera.ConvertWorldToScreen(new Graphics.Point(i - camera.Transform.Position.X, size.Width - camera.Transform.Position.Y));
+				var p2 = camera.ConvertWorldToScreen(new Graphics.Point(i - camera.Transform.Position.X, -size.Width - camera.Transform.Position.Y));
+				var p3 = camera.ConvertWorldToScreen(new Graphics.Point(size.Width - camera.Transform.Position.X, i - camera.Transform.Position.Y));
+				var p4 = camera.ConvertWorldToScreen(new Graphics.Point(-size.Width - camera.Transform.Position.X, i - camera.Transform.Position.Y));
+
+				Binding.SDL.SDL.SDL_RenderDrawLineF(Game.RenderContext, (float)p1.X, (float)p1.Y, (float)p2.X, (float)p2.Y);
+				Binding.SDL.SDL.SDL_RenderDrawLineF(Game.RenderContext, (float)p3.X, (float)p3.Y, (float)p4.X, (float)p4.Y);
+			}
+
+			var cx1 = camera.ConvertWorldToScreen(new Graphics.Point(0 - camera.Transform.Position.X, size.Width - camera.Transform.Position.Y));
+			var cy1 = camera.ConvertWorldToScreen(new Graphics.Point(0 - camera.Transform.Position.X, -size.Width - camera.Transform.Position.Y));
+			var cx2 = camera.ConvertWorldToScreen(new Graphics.Point(size.Width - camera.Transform.Position.X, 0 - camera.Transform.Position.Y));
+			var cy2 = camera.ConvertWorldToScreen(new Graphics.Point(-size.Width - camera.Transform.Position.X, 0 - camera.Transform.Position.Y));
+
+			Binding.SDL.SDL.SDL_SetRenderDrawColor(Game.RenderContext, 200, 200, 200, 0);
+			Binding.SDL.SDL.SDL_RenderDrawLineF(Game.RenderContext, (float)cx1.X, (float)cx1.Y, (float)cy1.X, (float)cy1.Y);
+			Binding.SDL.SDL.SDL_SetRenderDrawColor(Game.RenderContext, 200, 200, 200, 0);
+			Binding.SDL.SDL.SDL_RenderDrawLineF(Game.RenderContext, (float)cx2.X, (float)cx2.Y, (float)cy2.X, (float)cy2.Y);
+			Binding.SDL.SDL.SDL_SetRenderDrawColor(Game.RenderContext, color.R, color.G, color.B, color.A);
 		}
 
 		public static void NewLine()
