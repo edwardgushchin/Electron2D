48799ac6b18094a8ece5d76dae8a71aa5b3abca7
Eduard Gushchin <eduardgushchin@yandex.ru>
2025-02-23 23:57:52 +0300

    Added Component.cs

---
+++ Electron2D/Component.cs
@@ -0,0 +1,14 @@
+﻿namespace Electron2D;
+
+public abstract class Component
+{
+    protected internal GameObject? Owner { get; set; }
+
+    protected internal abstract void Awake();
+    
+    protected internal abstract void Start();
+    
+    protected internal abstract void Update(float deltaTime);
+    
+    protected internal abstract void OnDestroy();
+}
