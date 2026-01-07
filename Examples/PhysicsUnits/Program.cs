244e5b6f63e4d244baf2a7c68f6d90bbe367f74c
Eduard Gushchin <eduardgushchin@yandex.ru>
2026-01-07 22:44:23 +0300

    Пример работы физики

---
+++ Examples/PhysicsUnits/Program.cs
@@ -0,0 +1,24 @@
+﻿using Electron2D;
+
+namespace PhysicsUnits;
+
+internal static class Program
+{
+    public static void Main()
+    {
+        using var engine = new Engine(new EngineConfig
+        {
+            Window = new WindowConfig
+            {
+                Title = "Electron2D: Physics units demo",
+                Width = 800,
+                Height = 600
+            },
+            DebugGridEnabled = true,
+            VSync = VSyncMode.Enabled
+        });
+
+        engine.SceneTree.Root.AddChild(new PhysicsUnitsScene());
+        engine.Run();
+    }
+}
